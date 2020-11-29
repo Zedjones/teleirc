9a8a27812b04799106a52cc34c9002f5715ae886
Zedjones <dojoman19@gmail.com>
2020-11-29 12:07:35 -0500

    Modified build script to build for multiple architectures

--- build_binary.sh
+++ build_binary.sh
@@ -1,2 +1,12 @@
 CURRENT_VERSION=`git describe --tags`
-go build -ldflags "-X main.version=$CURRENT_VERSION" cmd/teleirc.go
+
+if [ $# -eq 1 ] && { [ $1 = "-a" ] || [ $1 = "--all" ]; }
+then
+    GOOS=linux GOARCH=amd64 go build -ldflags "-X main.version=$CURRENT_VERSION" -o output/teleirc-x86-linux cmd/teleirc.go
+    GOOS=openbsd GOARCH=amd64 go build -ldflags "-X main.version=$CURRENT_VERSION" -o output/teleirc-openbsd-linux cmd/teleirc.go
+    GOOS=windows GOARCH=amd64 go build -ldflags "-X main.version=$CURRENT_VERSION" -o output/teleirc-x86-windows.exe cmd/teleirc.go
+    GOOS=darwin GOARCH=amd64 go build -ldflags "-X main.version=$CURRENT_VERSION" -o output/teleirc-x86-darwin cmd/teleirc.go
+    GOOS=linux GOARCH=arm64 go build -ldflags "-X main.version=$CURRENT_VERSION" -o output/teleirc-arm64-linux cmd/teleirc.go
+else
+    go build -ldflags "-X main.version=$CURRENT_VERSION" cmd/teleirc.go
+fi
