564b5d1faa5c90f9e3189b4a5af6127c5f6c4cbd
Zedjones <dojoman19@gmail.com>
2020-11-29 20:42:18 -0500

    Fixed wrong name of OpenBSD

--- build_binary.sh
+++ build_binary.sh
@@ -4,7 +4,7 @@ if [ $# -ge 1 ] && { [ $1 = "-a" ] || [ $1 = "--all" ]; }
 then
     # Building different OSes for x86-64
     GOOS=linux GOARCH=amd64 go build -ldflags "-X main.version=$CURRENT_VERSION" -o output/teleirc-x86-linux cmd/teleirc.go
-    GOOS=openbsd GOARCH=amd64 go build -ldflags "-X main.version=$CURRENT_VERSION" -o output/teleirc-openbsd-linux cmd/teleirc.go
+    GOOS=openbsd GOARCH=amd64 go build -ldflags "-X main.version=$CURRENT_VERSION" -o output/teleirc-x86-openbsd cmd/teleirc.go
     GOOS=windows GOARCH=amd64 go build -ldflags "-X main.version=$CURRENT_VERSION" -o output/teleirc-x86-windows.exe cmd/teleirc.go
     GOOS=darwin GOARCH=amd64 go build -ldflags "-X main.version=$CURRENT_VERSION" -o output/teleirc-x86-darwin cmd/teleirc.go
     # Building arm binary for Linux
