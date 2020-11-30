69efb168e853039535f4482cc344bc3475bc1784
Zedjones <dojoman19@gmail.com>
2020-11-29 20:34:10 -0500

    Fixed issues with the cross compilation

--- build_binary.sh
+++ build_binary.sh
@@ -1,12 +1,14 @@
 CURRENT_VERSION=`git describe --tags`
 
-if [ $# -eq 1 ] && { [ $1 = "-a" ] || [ $1 = "--all" ]; }
+if [ $# -ge 1 ] && { [ $1 = "-a" ] || [ $1 = "--all" ]; }
 then
+    # Building different OSes for x86-64
     GOOS=linux GOARCH=amd64 go build -ldflags "-X main.version=$CURRENT_VERSION" -o output/teleirc-x86-linux cmd/teleirc.go
     GOOS=openbsd GOARCH=amd64 go build -ldflags "-X main.version=$CURRENT_VERSION" -o output/teleirc-openbsd-linux cmd/teleirc.go
     GOOS=windows GOARCH=amd64 go build -ldflags "-X main.version=$CURRENT_VERSION" -o output/teleirc-x86-windows.exe cmd/teleirc.go
     GOOS=darwin GOARCH=amd64 go build -ldflags "-X main.version=$CURRENT_VERSION" -o output/teleirc-x86-darwin cmd/teleirc.go
-    GOOS=linux GOARCH=arm64 go build -ldflags "-X main.version=$CURRENT_VERSION" -o output/teleirc-arm64-linux cmd/teleirc.go
+    # Building arm binary for Linux
+    GOOS=linux GOARCH=arm go build -ldflags "-X main.version=$CURRENT_VERSION" -o output/teleirc-arm-linux cmd/teleirc.go
 else
     go build -ldflags "-X main.version=$CURRENT_VERSION" cmd/teleirc.go
 fi
